30da6fa0f1c4a2af8b3158de6cfaf30511ffd2cf
talrez11 <tal@way2cu.com>
2015-01-26 15:24:01 +0200

    Added main.js

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,12 +1,23 @@
 /**
  * Main JavaScript
- * Site Name
+ * BusinessConsultant-Site
  *
  * Copyright (c) 2014. by Way2CU, http://way2cu.com
- * Authors:
+ * Authors:Tal Reznic
  */
+var Caracal = Caracal || {};
 
 function on_site_load() {
+
+	if ($('div.testimonial').length > 0) {
+
+		Caracal.testimonial_pages = new PageControl('div.testimonials_container', 'div.testimonial')
+		Caracal.testimonial_pages.attachNextControl($('a.arrow_next'))
+		Caracal.testimonial_pages.attachPreviousControl($('a.arrow_previous'))
+		.setInterval(5000)
+		.setWrapAround(true)
+		.setPauseOnHover(true);
+	}
 }
 
 $(on_site_load);
