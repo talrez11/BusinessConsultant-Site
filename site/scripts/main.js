b7c77f6794cbd05b0eaace076e0da304fef13ac6
Khalid Sawalhi <khalid@way2cu.com>
2015-02-12 13:31:27 +0200

    Scripts: Added testimonial switcher between with images.

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -7,17 +7,19 @@
  */
 var Caracal = Caracal || {};
 
+
 function on_site_load() {
 
-	if ($('div.testimonial').length > 0) {
 
+	// clubs section
+	$("div.img_as_list span").eq(0).addClass("active");
 		Caracal.testimonial_pages = new PageControl('div.testimonials_container', 'div.testimonial')
 		Caracal.testimonial_pages.attachNextControl($('a.arrow_next'))
 		Caracal.testimonial_pages.attachPreviousControl($('a.arrow_previous'))
+		Caracal.testimonial_pages.attachControls($('div.img_as_list span'))
 		.setInterval(5000)
 		.setWrapAround(true)
 		.setPauseOnHover(true);
-	}
 }
 
 $(on_site_load);
